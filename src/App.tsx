7fdeb4e9fcfa9bafc6621c742c533baa3abfc830
Andrii Ohiienko <andreyohiienko@gmail.com>
2023-06-11 00:19:29 -0700

    Add react query devtools and default options

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Main } from "./pages/Main";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <Main />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
 
